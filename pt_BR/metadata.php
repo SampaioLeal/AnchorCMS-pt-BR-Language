2e0cd4b7893eb00d33264512cc94a074e43fad38
SampL <samp14hd@gmail.com>
2017-08-03 13:04:27 -0300

    Update _explains and improvements

--- pt_BR/metadata.php
+++ pt_BR/metadata.php
@@ -10,34 +10,34 @@ return array(
 
 	// form fields
 	'sitename' => 'Nome do site',
-	'sitename_explain' => '',
+	'sitename_explain' => 'Digite o nome do seu site',
 	'sitename_missing' => 'Seu site precisa de um nome!',
 
 	'sitedescription' => 'Descrição do site',
-	'sitedescription_explain' => '',
+	'sitedescription_explain' => 'Digite a descrição do site',
 	'sitedescription_missing' => 'Seu site precisa de uma descrição!',
 
 	'homepage' => 'Página Inicial',
-	'homepage_explain' => '',
+	'homepage_explain' => 'A página inicial do site',
 
 	'postspage' => 'Página de Posts',
-	'postspage_explain' => '',
+	'postspage_explain' => 'Página onde estão localizados os posts',
 
 	'posts_per_page' => 'Posts por página',
-	'posts_per_page_explain' => '',
+	'posts_per_page_explain' => 'Quantos posts terão em uma página?',
 
 	'auto_publish_comments' => 'Permitir automaticamente comentários',
-	'auto_publish_comments_explain' => '',
+	'auto_publish_comments_explain' => 'Permitir que comentários sejam ativados automaticamente',
 
 	'comment_notifications' => 'Notificação por email para novos comentários',
-	'comment_notifications_explain' => '',
+	'comment_notifications_explain' => 'Sempre que alguém comentar você receberá um email',
 
 	'comment_moderation_keys' => 'Palavras-chave de Spam',
 	'comment_moderation_keys_explain' => 'Separe por vírgulas a lista de palavras-chave.<br>
 		Os comentários que conterem estas palavras serão automaticamente definidos como Spam.',
 
 	'current_theme' => 'Tema atual',
-	'current_theme_explain' => '',
+	'current_theme_explain' => 'Escolha o tema atual',
 
 	// messages
 	'updated' => 'Metadata atualizada',
